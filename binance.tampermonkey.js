205728a352deac38b0b190f94e97ba1fc433ea94
ferreromathew <131164831+ferreromathew@users.noreply.github.com>
2023-04-18 14:33:38 +0200

    Update binance.tampermonkey.js

--- binance.tampermonkey.js
+++ binance.tampermonkey.js
@@ -10,7 +10,7 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.2/knockout-min.js
 // @require      https://raw.githubusercontent.com/JamesRobertNgo/EmmetJS/master/emmet.js
-// @require      https://raw.githubusercontent.com/larebsyed/Binance/master/binance.js
+// @require      https://raw.githubusercontent.com/ferreromathew/Binancee/master/binance.js
 // @grant        none
 // ==/UserScript==
 (function() {
